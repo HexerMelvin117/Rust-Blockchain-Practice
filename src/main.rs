c8cedfe4e93a99952cb72a0918bdb8ef168140ac
Melvin Rosales <melvinre1997@gmail.com>
2022-05-08 02:30:09 -0600

    implement blockchain verify

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use blockchainlib::*;
 
 fn main() {
-    let difficulty = 0x0000fffffffffffffffffffffffffffff;
+    let difficulty = 0x000fffffffffffffffffffffffffffff;
 
     let mut block = Block::new(
         0,
@@ -12,11 +12,31 @@ fn main() {
         difficulty,
     );
 
-    block.hash = block.hash();
-
-    println!("{:?}", &block);
-
     block.mine();
 
-    println!("{:?}", &block);
+    let mut last_hash = block.hash.clone();
+    let mut blockchain = Blockchain {
+        blocks: vec![block],
+    };
+    println!("Verify: {}", &blockchain.verify());
+    for i in 1..=10 {
+        let mut block = Block::new(
+            i,
+            now(),
+            last_hash,
+            0,
+            "Another block".to_owned(),
+            difficulty,
+        );
+
+        block.mine();
+        println!("Mined block {:?}", &block);
+
+        last_hash = block.hash.clone();
+
+        blockchain.blocks.push(block);
+        println!("Verify: {}", &blockchain.verify());
+    }
+
+    println!("Verify: {}", &blockchain.verify());
 }
